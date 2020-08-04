6cd0a56f2c9f30af36014e6e8617233cc08eb359
Sabah97 <sabah.ashraf97@gmail.com>
2020-08-05 03:12:52 +0600

    css functionalities added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+const api = {
+	key: 'dad47d257f10c22e7dcdbecee6ae2921',
+	base: 'https://api.openweathermap.org/data/2.5',
+};
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const dateBuilder = (d) => {
+		let months = [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December',
+		];
+		let days = [
+			'Sunday',
+			'Monday',
+			'Tuesday',
+			'Wednesday',
+			'Thursday',
+			'Friday',
+			'Saturday',
+		];
+		let day = days[d.getDay()];
+		let date = d.getDate();
+		let month = months[d.getMonth()];
+		let year = d.getFullYear();
+		return `${day} ${date} ${month} ${year}`;
+	};
+	return (
+		<div className='App'>
+			<main>
+				<div className='search-box'>
+					<input type='text' className='search-bar' placeholder='Search...' />
+				</div>
+				<div className='location-box'>
+					<div className='location'>Dhaka,Bangladesh</div>
+					<div className='date'>{dateBuilder(new Date())}</div>
+				</div>
+				<div className='weather-box'>
+					<div className='temp'>15°C</div>
+					<div className='weather'>Sunny</div>
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
